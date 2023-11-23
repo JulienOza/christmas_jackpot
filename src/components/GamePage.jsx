5c68357509ec45b28a8fe325186fb6845d0865a7
Pierre-Adrien Gouillart <pagouillart@hotmail.fr>
2023-11-23 17:00:20 +0100

    routes added with components

---
+++ src/components/GamePage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/NavBar/Navbar";
+import PopUpFinal from "./PopUpFinal/PopUpFinal";
+
+function GamePage() {
+  return (
+    <>
+      <Navbar />
+      <PopUpFinal />
+    </>
+  );
+}
+
+export default GamePage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import HomePage from './components/HomePage.jsx';
+import GamePage from './components/GamePage.jsx';
 
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/GamePage",
+          element: <GamePage />,
+        },
+      ],
+    },
+  ]
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+          <RouterProvider router={router} />
   </React.StrictMode>,
 )
